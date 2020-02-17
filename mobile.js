087ea57bba6e6c86cdfbb1a7c9a29e2b3633113b
Joao <joao.craveiro@valsys.io>
2020-02-17 12:36:07 +0000

    new structure

---
+++ mobile.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+  var menu = document.getElementById("menu-mobile");
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+}
